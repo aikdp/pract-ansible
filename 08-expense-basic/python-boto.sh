02fdb0239b029c8694b67ff8dde4919a11ef926e
aikdp <kdprasad028@gmail.com>
2025-04-06 19:41:30 +0530

    ansible practice

---
+++ 08-expense-basic/python-boto.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#FOR UBUNTU MACHINE
+# 1. Update your package index
+sudo apt update
+
+# 2. Install required dependencies
+sudo apt install unzip curl -y
+
+# 3. Download the AWS CLI v2 installer
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# 4. Unzip the installer
+unzip awscliv2.zip
+
+# 5. Run the installer
+sudo ./aws/install
+
+rm -rf awscliv2.zip aws/
+
+# 6. Confirm installation
+aws --version
